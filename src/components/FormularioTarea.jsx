f21dd88f76e11a92f12b5649de61c669ba8d5d30
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-26 18:29:42 -0300

    maquetado terminado

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,12 +1,15 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTarea from "./ListaTarea";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+    const [tarea, setTarea] = useState("");
+    const [tareas, setTareas] = useState([]);
   return (
     <>
     <Form>
       <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="Ingrese una tarea" />
+        <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=>console.log(e.target.value)} />
         <Button variant="primary">Enviar</Button>
       </Form.Group>
     </Form>
