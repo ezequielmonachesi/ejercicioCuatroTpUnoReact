822d0fd49d04dc5c4eef095781b7af2351299eb0
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-27 09:06:18 -0300

    logica de boton submit terminado

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -3,22 +3,29 @@ import ListaTarea from "./ListaTarea";
 import { useState } from "react";
 
 const FormularioTarea = () => {
-    const [tarea, setTarea] = useState("");
-    const [tareas, setTareas] = useState([]);
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-
-    }
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTareas([...tareas, tarea]);
+    setTarea("");
+  };
   return (
     <>
-    <Form onSubmit={handleSubmit}>
-        {/* 2:11 */}
-      <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=>setTarea(e.target.value)} />
-        <Button variant="primary" type="submit">Enviar</Button>
-      </Form.Group>
-    </Form>
-    <ListaTarea></ListaTarea>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListaTarea></ListaTarea>
     </>
   );
 };
