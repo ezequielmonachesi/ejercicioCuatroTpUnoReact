e6132ac17be105ed465cdbeefc80b528ed7b917d
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-27 09:19:59 -0300

    se recupero lo borrado

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,11 +1,12 @@
 import { Button, ListGroup } from "react-bootstrap";
 
-const ItemTarea = ({array}) => {
-    return (
-          <Button variant="danger">Borrar</Button>
-        </ListGroup.Item>
-      }
-    );
+const ItemTarea = () => {
+  return (
+      <ListGroup.Item className="d-flex justify-content-between">
+        Ejemplo de tarea
+        <Button variant="danger">Borrar</Button>
+      </ListGroup.Item>
+  );
 };
 
 export default ItemTarea;
